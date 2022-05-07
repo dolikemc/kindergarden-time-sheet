278a07238e9ca02dfe2dde403c76818c083c1ad0
christoph <chr@buchetmann.de>
2022-05-07 11:16:43 +0200

    add start date

--- src/DataRow.py
+++ src/DataRow.py
@@ -38,55 +38,59 @@ class DateHandler:
         self._config = config.config
         self.font = Font(size=16)
         self.side = Side(style='medium', color='00000000')
-
+        self.year = self._config.get('year', datetime.today().year)
         self._calendar = Calendar(firstweekday=0)
         self._holidays: Dict = holidays.country_holidays(
             country=self._config.get('country', 'DE'),
             subdiv=self._config.get('subdiv', 'BY'),
-            years=self._config.get('year', datetime.today().year)
+            years=self.year
         )
+        self.date_format = self._config.get('format', '%d/%m')
+        delay_value = self._config.get('update_delay', 0)
+        if delay_value > 0:
+            self.update_delay = f"-{delay_value}"
+        else:
+            self.update_delay = ""
+        self.overtime_factor = self._config.get('overtime_factor', 1.5)
+        self.styles: List = self._config.get('styles', ['Output', 'Output', 'Output'])
+
         self._special_days = {}
         for name in self._config.get('holidays', [{'name': ''}]):
             for date_record in name.get('dates', []):
-                month_day = datetime.strptime(date_record, self._config.get('format', '%d/%m'))
-                key_date = date(year=self._config.get('year', datetime.today().year),
-                                month=month_day.month, day=month_day.day)
+                month_day = datetime.strptime(date_record, self.date_format)
+                key_date = date(year=self.year, month=month_day.month, day=month_day.day)
                 self._special_days[key_date.isoformat()] = name.get('name', '')
 
     def year_iterator(self) -> Iterator[DataRow]:
-        day = date(year=self._config.get('year', date.today().year), month=1, day=1)
+        day = date(year=self.year, month=1, day=1)
         return_row = DataRow()
-        for i in range(0, (date(year=self._config.get('year', date.today().year) + 1, month=1, day=1) -
-                           date(year=self._config.get('year', date.today().year), month=1, day=1)).days):
+        for i in range(0, (date(year=self.year + 1, month=1, day=1) - date(year=self.year, month=1, day=1)).days):
             if day in self._holidays:
-                return_row.date, return_row.type, return_row.name = day, 2, self._config.get(
-                    'holiday', 'Holiday')
+                return_row.date, return_row.type, return_row.name = day, 2, self._config.get('holiday', 'Holiday')
             elif day.weekday() > 4:
                 return_row.date, return_row.type, return_row.name = day, 1, ''
             elif day.isoformat() in self._special_days.keys():
-                return_row.date, return_row.type, return_row.name = day, 3, self._special_days[
-                    day.isoformat()]
+                return_row.date, return_row.type, return_row.name = day, 3, self._special_days[day.isoformat()]
             else:
                 return_row.date, return_row.type = day, 0
             yield return_row
             day += timedelta(days=1)
 
-    def add_row(self, hours: List, stop: str = '') -> int:
+    def add_row(self, hours: List, stop: str = '', start: str = '') -> int:
         if len(hours) < 5:
             raise Exception('you have to provide hours for each weekday')
 
         dv = self.create_header()
-        month_day = self.determine_stop_date(stop)
+        start_day = self.determine_stop_date(start)
+        stop_date = self.determine_stop_date(stop)
         for index, day_row in enumerate(self.year_iterator()):
-
             # stop criteria for a member
-            if day_row.date > month_day:
+            if day_row.date > stop_date:
                 return 1
 
             if day_row.type in (1, 2, 3):
                 for c in range(1, 8):
-                    self._worksheet.cell(row=index + 2, column=c).style = \
-                        self._config.get('styles', ['Output', 'Output', 'Output'])[day_row.type - 1]
+                    self._worksheet.cell(row=index + 2, column=c).style = self.styles[day_row.type - 1]
                 # self._worksheet.cell(row=index + 2, column=6, value=day_row.name)
                 # self._worksheet.cell(row=index + 2, column=5, value="00:00")
 
@@ -94,21 +98,25 @@ class DateHandler:
                 self._worksheet.cell(row=index + 2, column=4, value='00:00')
                 self._worksheet.cell(row=index + 2, column=4).number_format = '[hh]:mm'
 
-                self._worksheet.cell(row=index + 2, column=3, value=f"0{hours[day_row.date.weekday()]}:00")
-                self._worksheet.cell(
-                    row=index + 2, column=5,
-                    value=f'=IF(AND(A{index + 2}<TODAY()-2,C{index + 2}<>"",F{index + 2}=""),'
-                          f'IF(C{index + 2}=0,D{index + 2}*1.5,'
-                    #      f'D{index + 2}-C{index + 2}'
-                          f'TEXT(ABS(D{index + 2}-C{index + 2}),'
-                          f'IF(_xlfn.NUMBERVALUE(D{index + 2})<_xlfn.NUMBERVALUE(C{index + 2}),"-","")&"[hh]:mm")'
-                          f'),"")')
-                self._worksheet.cell(
-                    row=index + 2, column=17,
-                    value=f'=IF(AND(A{index + 2}<TODAY()-2,C{index + 2}<>"",F{index + 2}=""),'
-                          f'IF(C{index + 2}=0,D{index + 2}*1.5,'
-                          f'D{index + 2}-C{index + 2}'
-                          f'),"")')
+                if day_row.date < start_day:
+                    self._worksheet.cell(row=index + 2, column=3, value="00:00")
+                    self._worksheet.cell(row=index + 2, column=5, value="0")
+                    self._worksheet.cell(row=index + 2, column=17, value="0")
+                else:
+                    self._worksheet.cell(row=index + 2, column=3, value=f"0{hours[day_row.date.weekday()]}:00")
+                    self._worksheet.cell(
+                        row=index + 2, column=5,
+                        value=f'=IF(AND(A{index + 2}<TODAY(){self.update_delay},C{index + 2}<>"",F{index + 2}=""),'
+                              f'IF(C{index + 2}=0,D{index + 2}*{self.overtime_factor},'
+                              f'TEXT(ABS(D{index + 2}-C{index + 2}),'
+                              f'IF(_xlfn.NUMBERVALUE(D{index + 2})<_xlfn.NUMBERVALUE(C{index + 2}),"-","")&"[hh]:mm")'
+                              f'),"")')
+                    self._worksheet.cell(
+                        row=index + 2, column=17,
+                        value=f'=IF(AND(A{index + 2}<TODAY(){self.update_delay},C{index + 2}<>"",F{index + 2}=""),'
+                              f'IF(C{index + 2}=0,D{index + 2}*{self.overtime_factor},'
+                              f'D{index + 2}-C{index + 2}'
+                              f'),"")')
 
                 dv.add(self._worksheet.cell(row=index + 2, column=6))
 
@@ -135,12 +143,10 @@ class DateHandler:
 
     def determine_stop_date(self, stop):
         if stop:
-            month_day = datetime.strptime(stop, self._config.get('format', '%d/%m'))
+            month_day = datetime.strptime(stop, self.date_format)
         else:
-            month_day = date(year=self._config.get('year', datetime.today().year), month=12, day=31)
-        return date(year=self._config.get('year', datetime.today().year),
-                    month=month_day.month,
-                    day=month_day.day)
+            month_day = date(year=self.year, month=12, day=31)
+        return date(year=self.year, month=month_day.month, day=month_day.day)
 
     def create_header(self):
         dv = DataValidation(type='list', formula1='"krank,urlaub,kindkrank,fortbildung"')
@@ -166,9 +172,9 @@ class DateHandler:
         self._worksheet.merge_cells(start_row=10, start_column=8, end_row=10, end_column=12)
         self.set_cell_std_format(from_row=10, from_column=8, style_number=3, text='Urlaubstage')
         self.set_cell_std_format(from_row=11, from_column=8, style_number=3,
-                                 text=f"Rest {self._config.get('year', datetime.today().year) - 1}")
+                                 text=f"Rest {self.year - 1}")
         self.set_cell_std_format(from_row=11, from_column=9, style_number=3,
-                                 text=f"Soll {self._config.get('year', datetime.today().year)}")
+                                 text=f"Soll {self.year}")
         self.set_cell_std_format(from_row=11, from_column=10, style_number=3, text="Summe")
         self.set_cell_std_format(from_row=11, from_column=11, style_number=3, text="Genommen")
         self.set_cell_std_format(from_row=11, from_column=12, style_number=3, text="Offen")
@@ -177,9 +183,9 @@ class DateHandler:
         self._worksheet.merge_cells(start_row=10, start_column=14, end_row=10, end_column=16)
         self.set_cell_std_format(from_row=10, from_column=14, style_number=3, text='Überstunden')
         self.set_cell_std_format(from_row=11, from_column=14, style_number=3,
-                                 text=f"Rest {self._config.get('year', datetime.today().year) - 1}")
+                                 text=f"Rest {self.year - 1}")
         self.set_cell_std_format(from_row=11, from_column=15, style_number=3,
-                                 text=f"{self._config.get('year', datetime.today().year)}")
+                                 text=f"{self.year}")
         self.set_cell_std_format(from_row=11, from_column=16, style_number=3, text="Summe")
 
         # holiday cells
@@ -189,8 +195,9 @@ class DateHandler:
         self.set_cell_std_format(from_row=12, from_column=11, text='=COUNTIF(F2:F367,"urlaub")')
         self.set_cell_std_format(from_row=12, from_column=12, text="=J12-K12")
 
-        # overtiem cells
-        # =TEXT(ABS(SUMME((-1+2*(LINKS(G11:G22)<>"-"))*(RECHTS(0&G11:G22;5))));WENN(SUMME((-1+2*(LINKS(G11:G22)<>"-"))*(RECHTS(0&G11:G22;5)))<0;"-";)&"[hh]:mm:ss")
+        # overtime cells
+        # =TEXT(ABS(SUMME((-1+2*(LINKS(G11:G22)<>"-"))*(RECHTS(0&G11:G22;5))));WENN(SUMME((-1+2*(LINKS(G11:G22)<>"-"))
+        # *(RECHTS(0&G11:G22;5)))<0;"-";)&"[hh]:mm:ss")
         self.set_cell_std_format(from_row=12, from_column=14, text="00:00", number_format='[hh]":"mm')
         self.set_cell_std_format(from_row=12, from_column=15,
                                  text=
@@ -211,21 +218,18 @@ class DateHandler:
         # header training
         self._worksheet.merge_cells(start_row=14, start_column=8, end_row=14, end_column=10)
         self.set_cell_std_format(from_row=14, from_column=8, style_number=3, text='Fortbildung')
-        self.set_cell_std_format(from_row=15, from_column=8, style_number=3,
-                                 text=f"Soll {self._config.get('year', datetime.today().year)}")
+        self.set_cell_std_format(from_row=15, from_column=8, style_number=3, text=f"Soll {self.year}")
         self.set_cell_std_format(from_row=15, from_column=9, style_number=3, text=f"Genommen")
         self.set_cell_std_format(from_row=15, from_column=10, style_number=3, text="Offen")
 
         # header sick
         self.set_cell_std_format(from_row=14, from_column=12, style_number=3, text="Krankheitstage")
-        self.set_cell_std_format(from_row=15, from_column=12, style_number=3,
-                                 text=f"{self._config.get('year', datetime.today().year)}")
+        self.set_cell_std_format(from_row=15, from_column=12, style_number=3, text=f"{self.year}")
 
         # header child sick
         self._worksheet.merge_cells(start_row=14, start_column=14, end_row=14, end_column=16)
         self.set_cell_std_format(from_row=14, from_column=14, style_number=3, text='Kinder Krankeitstage')
-        self.set_cell_std_format(from_row=15, from_column=14, style_number=3,
-                                 text=f"Soll {self._config.get('year', datetime.today().year)}")
+        self.set_cell_std_format(from_row=15, from_column=14, style_number=3, text=f"Soll {self.year}")
         self.set_cell_std_format(from_row=15, from_column=15, style_number=3, text="Genommen")
         self.set_cell_std_format(from_row=15, from_column=16, style_number=3, text="Offen")
 
@@ -248,7 +252,7 @@ class DateHandler:
                             number_format: str = None) -> Cell:
         cell: Cell = self._worksheet.cell(row=from_row, column=from_column)
         if style_number:
-            cell.style = self._config.get('styles', ['Output', 'Output', 'Output', 'Output'])[style_number]
+            cell.style = self.styles[style_number]
         if text:
             cell.value = text
         if number_format in ['d/m', '0.00', '[hh]":"mm']:
